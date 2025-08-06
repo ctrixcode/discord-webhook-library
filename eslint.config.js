b91bcfee5fa27d3fa9b681ad3292d67800e0610d
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-08-06 09:38:07 +0530

    feat: added prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules', 'eslint.config.js', 'vite.config.ts'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  }
+);
