cdff994dd306c9d3d264c80b01af3f4f113329cb
Chandrashekhar Kachawa <42296139+ctrixcode@users.noreply.github.com>
2025-08-12 18:22:21 +0530

    feat: generate ts types

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import dts from 'vite-plugin-dts'; // Import the plugin
 
 export default defineConfig({
   build: {
@@ -17,4 +18,5 @@ export default defineConfig({
       },
     },
   },
+  plugins: [dts()], // Add the plugin here
 });
